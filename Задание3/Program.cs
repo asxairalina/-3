aa01f8ea16acdb480594112aa957a28f3afe9f6d
212227 <212227@1220-012-22>
2023-10-23 10:44:39 +0300

    Добавьте файлы проекта.

---
+++ Задание3/Program.cs
@@ -0,0 +1,22 @@
+﻿int a;
+int b;
+int c;
+
+while (true)
+{
+    a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
+    c = Convert.ToInt32(Console.ReadLine());
+    if (a<100000 && b < 100000 && c < 100000)
+    {
+        if ((a +b > c) && (b + c > a) && (a + c > b) )
+        {
+            Console.WriteLine("Треугольник существует");
+        }
+
+        else { Console.WriteLine("Треугольник не существует");
+        }
+            
+    }
+
+}
